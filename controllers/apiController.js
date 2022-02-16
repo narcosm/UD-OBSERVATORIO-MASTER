a0f36f3fe19e40dbe054052b6578d5e09148d5c1
narcosm <narcosm@correo.udistrital.edu.co>
2022-02-16 17:52:16 -0500

    variables quemadas

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -12,8 +12,8 @@ apiController.callback = async (req, res) => {
     const email = emails[0].value
     const user = await userClient.getUsers(email)
     if (user.length) {
-        // return res.redirect(`https://observatorio.planestic.udistrital.edu.co/login?token=${token}&name=${displayName}&url_image=${image}&rol=${user[0].rol}&email=${user[0].email}`)
-        return res.redirect(`http://192.168.0.6:8080/login?token=${token}&name=${displayName}&url_image=${image}&rol=${user[0].rol}&email=${user[0].email}`)
+        return res.redirect(`https://observatorio.planestic.udistrital.edu.co/login?token=${token}&name=${displayName}&url_image=${image}&rol=${user[0].rol}&email=${user[0].email}`)
+       // return res.redirect(`http://192.168.0.6:8080/login?token=${token}&name=${displayName}&url_image=${image}&rol=${user[0].rol}&email=${user[0].email}`)
     } else {
         const newUser = await userClient.addUsers({
             name: displayName,
@@ -25,8 +25,8 @@ apiController.callback = async (req, res) => {
         const user = await userClient.getUsers(email)
         log.info(`el nuevo user ${user} -------------------------`)
         if (user.length) {
-            // return res.redirect(`https://observatorio.planestic.udistrital.edu.co/login?token=${token}&name=${displayName}&url_image=${image}&rol=${user[0].rol}&email=${user[0].email}`)
-            return res.redirect(`http://192.168.0.6:8080/login?token=${token}&name=${displayName}&url_image=${image}&rol=${user[0].rol}&email=${user[0].email}`)
+            return res.redirect(`https://observatorio.planestic.udistrital.edu.co/login?token=${token}&name=${displayName}&url_image=${image}&rol=${user[0].rol}&email=${user[0].email}`)
+            //return res.redirect(`http://192.168.0.6:8080/login?token=${token}&name=${displayName}&url_image=${image}&rol=${user[0].rol}&email=${user[0].email}`)
         } else {
             return res.status(BAD_REQUEST).json({ error: 'user error' })
         }
